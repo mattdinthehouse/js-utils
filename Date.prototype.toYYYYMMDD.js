17e3f88d88cb4e881814faf2aabb53866eb38322
Matt D <mattsd1410@gmail.com>
2016-06-21 13:04:22 +1000

    getMonth() returns 0-11 when this should output 1-12

--- Date.prototype.toYYYYMMDD.js
+++ Date.prototype.toYYYYMMDD.js
@@ -1,7 +1,7 @@
 Date.prototype.toYYYYMMDD = function() {
   // Depends on String.prototype.leftPad
   var year = this.getFullYear();
-  var month = (''+this.getMonth()).leftPad('0', 2);
+  var month = (''+(this.getMonth() + 1)).leftPad('0', 2);
   var date = (''+this.getDate()).leftPad('0', 2);
 
   return year+'-'+month+'-'+date;
