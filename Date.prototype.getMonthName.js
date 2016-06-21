be280dbfa68c1625ff1e3ea363d4d451139c2b2e
Matt D <mattsd1410@gmail.com>
2016-06-21 13:06:46 +1000

    Added Date.prototype.getMonthName

---
+++ Date.prototype.getMonthName.js
@@ -0,0 +1,18 @@
+Date.prototype.getMonthName = function() {
+  var months = [
+    'January',
+    'February',
+    'March',
+    'April',
+    'May',
+    'June',
+    'July',
+    'August',
+    'September',
+    'October',
+    'November',
+    'December'
+  ];
+
+  return months[this.getMonth()];
+}
