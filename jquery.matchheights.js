c08171563d78df318b7a80820fbb0cb47ce2bc5e
Matt D <mattsd1410@gmail.com>
2016-08-18 11:10:40 +1000

    Added data-matchheights-noapply and ability to run on specific container

--- jquery.matchheights.js
+++ jquery.matchheights.js
@@ -1,10 +1,30 @@
-function matchheights() {
-	$('[data-matchheights]').each(function(_, container) {
-		container = $(container);
+function matchheights(container) {
+	if(container === undefined) {
+		// Run on all containers
+		$('[data-matchheights]').each(function(_, container) {
+			domatching($(container));
+		});
+	}
+	else if(container instanceof jQuery) {
+		// Run on supplied container
+		domatching(container);
+	}
+	else {
+		// Supplied container is invalid
+		console.error('Supplied invalid container to matchheights. Type must be jQuery, got '+(typeof container));
+	}
+
+	function domatching(container) {
+		// Get all the elements to look at
 		var elements = container.find(container.attr('data-matchheights'));
+		if(elements.length < 1)
+			return;
+
+		// Get the subset list of elements to apply height changes to
+		var affectedElements = elements.filter(':not([data-matchheights-noapply])');
 
 		// Reset the heights of everything
-		elements.css('height', 'auto');
+		affectedElements.css('height', 'auto');
 
 		if(container.attr('data-matchheights-mediaquery') !== undefined) {
 			if(!window.matchMedia(container.attr('data-matchheights-mediaquery')).matches) {
@@ -25,11 +45,15 @@ function matchheights() {
 		});
 
 		// Apply the height
-		elements.css('height', maxHeight+'px');
-	});
+		affectedElements.css('height', maxHeight+'px');
+	}
 }
-jQuery(window).load(matchheights); // For chrome
+jQuery(window).load(function(event) {
+	matchheights()
+});
 jQuery(document).ready(function($) {
-	$(window).resize(matchheights);
+	$(window).resize(function(event) {
+		matchheights();
+	});
 	matchheights();
 });
